97f16029b7aa9d147573de0a12b3631aa1662fc1
luckace82 <lakeshshrestha82@gmail.com>
2024-01-21 22:35:49 +0545

    minor changes

--- 1/main.py
+++ 1/main.py
@@ -1,19 +1,4 @@
 # from typing import Union
-# from fastapi import FastAPI
-# app=FastAPI()
-
-# @app.get("/")
-# def read_root():
-#     return{"hello":"world"}
-
-# app.get("/items/{iteem_id}")
-# def read_item(item_id:int,q:Union[str,None]=None):
-#     return{"item_id":item_id ,"q":q}
-
-
-
-
-# from typing import Union
 
 # from fastapi import FastAPI
 
